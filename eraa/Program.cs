083f0fcd8422cfbb15e50bb620a0073b31dc3c62
moe's Pc <m.hassan.dev@icloud.com>
2026-08-12 17:48:54 +0300

    Add project files.

---
+++ eraa/Program.cs
@@ -0,0 +1,14 @@
+﻿using eraa.Data;
+
+namespace EFCoreCodeFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using BikeStores532Context db = new BikeStores532Context();
+
+            Console.WriteLine("Database Ready");
+        }
+    }
+}
